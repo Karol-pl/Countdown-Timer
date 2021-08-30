edf961b2db1e4b05c65606e2d57e49050bf17430
Karol Homziuk <karolhomziuk@gmail.com>
2021-08-30 17:24:30 +0200

    add new features

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # Countdown-Timer
- Simple, New year countdown timer made with Vanilla JS
- 
- ## SEE LIVE:
- ## https://karol-pl.github.io/Countdown-Timer/
- 
- ## **STACK:**
- * HTML
- * CSS3
- * JS ES6
- 
- ## **WHAT IT DOES:**
- 1. Countdown timer displays months, days, hours, minutes and seconds remaining till NYE.
- 2. Depending of numbers displayed it switches the text below it to singular/plural form.
+
+Countdown timer made using vanilla JS!
+
+1. Pick a name of event
+2. Pick a date to countdown to
+3. Switch background photo that suits the occasion
+4. Click save and enjoy the time left!
+
+## SEE LIVE:
+
+## https://karol-pl.github.io/Countdown-Timer/
+
+## **STACK:**
+
+- HTML
+- CSS3
+- JS ES6
+
+## **WHAT IT DOES:**
+
+1.  Countdown timer displays months, days, hours, minutes and seconds remaining till date of your choosing.
+2.  Depending of numbers displayed in counter, it switches the text below it to singular/plural form.
+3.  It's responsive.
+4.  You can manipulate:
+
+- event name,
+- event date,
+- background image
--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// get settings and buttons
+const settings = document.querySelector(".settings");
+const settingsBtn = document.querySelector(".settings-btn");
+const saveBtn = document.querySelector(".save");
+
+// get inputs
+const eventName = document.querySelector("#event-name");
+const eventDay = document.querySelector("#event-day");
+const eventMonth = document.querySelector("#event-month");
+const eventYear = document.querySelector("#event-year");
+const eventImg = document.querySelector("#event-image");
+
+// get h1 span
+const eventSpan = document.querySelector(".event");
+
+//get body to change bg image
+const body = document.getElementsByTagName("body")[0];
+
 // get paragraphs where numbers gonna be held
 const monthsCount = document.getElementById("months");
 const daysCount = document.getElementById("days");
@@ -11,56 +29,82 @@ const hourText = document.getElementById("hour-title");
 const minuteText = document.getElementById("minute-title");
 const secondText = document.getElementById("second-title");
 
-const newYears = "Jan 1, 2022 0:00:00";
+const newYear = "Jan 1, 2022 0:00:00";
+let usersTime;
 
-function countdown() {
-  const newYearsDate = new Date(newYears);
-  const currentDate = new Date();
+const setTime = () => {
+  const newYearDate = new Date(newYear);
+  const currentTime = new Date();
+  const result = usersTime - currentTime;
+  console.log(usersTime);
 
-  //get months 
-  const newYearsDateYear = newYearsDate.getFullYear();
-  const currentDateYear = currentDate.getFullYear();
-  let newYearsDateMonth = newYearsDate.getMonth();
-  let currentDateMonth = currentDate.getMonth();
-  if (newYearsDateMonth === 0) {
-    newYearsDateMonth++;
-    currentDateMonth++;
-  }
+  const days = Math.floor(result / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((result / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((result / (1000 * 60)) % 60);
+  const seconds = Math.floor((result / 1000) % 60);
 
-  const timeLeft = newYearsDate - currentDate;
+  const userYears = eventYear.value;
+  const currentYears = currentTime.getFullYear();
+  let userMonths = eventMonth.value;
+  let currentMonths = currentTime.getMonth();
+  currentMonths++;
 
-  const seconds = Math.floor((timeLeft / 1000) % 60);
-  const minutes = Math.floor((timeLeft / (1000 * 60)) % 60);
-  const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  // Months
   const months =
-    (newYearsDateYear - currentDateYear) * 12 +
-    (newYearsDateMonth - currentDateMonth) -
-    1;
-  // update the numbers in secs,mins etc + determine title if its singular or plural depending on number
-  secondsCount.innerHTML = timeFormat(seconds);
-  seconds === 1
-    ? (secondText.innerHTML = "second")
-    : (secondText.innerHTML = "seconds");
-  minutesCount.innerHTML = timeFormat(minutes);
-  minutes === 1
-    ? (minuteText.innerHTML = "minute")
-    : (minuteText.innerHTML = "minutes");
-  hoursCount.innerHTML = hours;
-  hours === 1 ? (hourText.innerHTML = "hour") : (hourText.innerHTML = "hours");
-  daysCount.innerHTML = days;
-  days === 1 ? (dayText.innerHTML = "day") : (dayText.innerHTML = "days");
-  monthsCount.innerHTML = months;
-  months === 1
-    ? (monthText.innerHTML = "month")
-    : (monthText.innerHTML = "months");
-}
+    (userYears - currentYears) * 12 + (userMonths - currentMonths) - 1;
 
-// function to put 0 before seconds and minutes so it looks better
+  secondsCount.textContent = timeFormat(seconds);
+  seconds === 1
+    ? (secondText.textContent = "second")
+    : (secondText.textContent = "seconds");
+  minutesCount.textContent = timeFormat(minutes);
+  minutes === 1
+    ? (minuteText.textContent = "minute")
+    : (minuteText.textContent = "minutes");
+  hoursCount.textContent = hours;
+  hours === 1
+    ? (hourText.textContent = "hour")
+    : (hourText.textContent = "hours");
+  daysCount.textContent = days;
+  days === 1 ? (dayText.textContent = "day") : (dayText.textContent = "days");
+  monthsCount.textContent = months;
+  months === 1
+    ? (monthText.textContent = "month")
+    : (monthText.textContent = "months");
+};
+
+const appUpdate = () => {
+  if (eventSpan.textContent === "") {
+    eventSpan.textContent = "New year ";
+  } else {
+    eventSpan.textContent = eventName.value;
+  }
+  usersTime = new Date(
+    `${eventMonth.value} ${eventDay.value} ${eventYear.value}`
+  );
+  if (eventImg.value === "") {
+    body.style.backgroundImage =
+      "url('./assets/ben-white-xqjMjaGGhmw-unsplash.jpg')";
+  } else {
+    body.style.backgroundImage = `url('${eventImg.value}')`;
+  }
+  setTime();
+};
+
+appUpdate();
+
+settingsBtn.addEventListener("click", () => {
+  settings.classList.toggle("active");
+  settingsBtn.classList.toggle("open");
+});
+
+saveBtn.addEventListener("click", appUpdate);
+saveBtn.addEventListener("click", () => {
+  settings.classList.toggle("active");
+  settingsBtn.classList.toggle("open");
+});
+
 function timeFormat(time) {
   return time < 10 ? `0${time}` : time;
 }
-
-setInterval(() => {
-  countdown();
-}, 1000);
+setInterval(setTime, 1000);
